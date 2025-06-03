08660977f375606a23a8e8d045d4b12f05a59da0
DeBujos <55712332+mrohadi@users.noreply.github.com>
2025-06-03 08:13:03 +0700

    Order (#6)
    * [mrohadi] - feat: setup order services with clean architecture with ddd
    
    * [mrohadi] - feat: add order domain
    
    * [mrohadi] - feat: add order infrastrcture
    
    * [mrohadi] - feat: add order application
    
    * [mrohadi] - feat: add order api (presentation)

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using BuildingBlocks.Behaviours;
+
+namespace Ordering.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            config.AddOpenBehavior(typeof(LogginBehaviour<,>));
+        });
+
+        return services;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Ordering.Application.Exceptions;
+
+public class OrderNotFoundException(Guid id) : NotFoundException("Order", id)
+{
+}
