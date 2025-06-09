497d7dcc243a7f446870dd393bc3d482682b61d3
mrohadi <muhammadrohadi03@gmail.com>
2025-06-09 15:35:31 +0700

    [mrohadi] - feat: finish message broker

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -12,3 +12,5 @@ global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
 global using Basket.API.Data;
 global using Basket.API.Exceptions;
+global using Basket.API.Dtos;
+global using BuildingBlocks.Messaging.MassTransit;
